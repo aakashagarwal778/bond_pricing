46f9837362bb6e240f225abaa545ac5075600e88
Aakash Agarwal <aakashagarwal778@gmail.com>
2025-01-29 22:45:56 +0100

    V 0.5

--- bond_pricing.py
+++ bond_pricing.py
@@ -22,3 +22,22 @@ def bond_pricing(face_value, coupon_rate, freq, yield_to_maturity, purchase_date
         else:
             raise ValueError("Compounding frequency can only be annual (1) or semiannual (2).")
 
+    t_list = []
+    for x in date_rows:
+        t_list.append((x - purchase_date).days / 365)
+
+    date_rows = [dt.strftime("%Y-%m-%d") for dt in date_rows]
+
+    c_rows = []
+    for x in t_list:
+        c_rows.append(np.round(face_value * coupon_rate, 2))
+    c_rows[-1] += face_value
+
+    pv_rows = []
+    for x, y in zip(c_rows, t_list):
+        pv_rows.append(np.round(x / (1 + yield_to_maturity) ** y, 2))
+
+    payments_data = [date_rows, c_rows, pv_rows]
+    payments_columns = ["Payment Date", "Coupon", "Present Value"]
+    payments_df = pd.DataFrame(payments_data, payments_columns)
+    payments_df = payments_df.transpose()
